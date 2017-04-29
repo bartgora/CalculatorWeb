00e9a42d18b4f6460067b419b27759be4aa80989
bartlomiej-gora <bartlomiej.gora@gmail.com>
2017-04-29 20:27:41 +0200

    #9/Added angular

--- src/main/resources/static/js/rpnCalculator.js
+++ src/main/resources/static/js/rpnCalculator.js
@@ -5,13 +5,17 @@ app.controller('controller', function($scope, $http){
         $scope.showErrors = false;
         $scope.calculate = function($http){
 
-        $http.get('/calculate' + $scope.source)
+        $http.({
+        method: 'GET',
+        url: '/calculate/' + $scope.source
+        })
                 .then(function(response){
                     $scope.result = response.result;
                 }, function(error){
                         $scope.errorCode = error.status
                         $scope.errorMessage = error.error;
                         $scope.showErrors = true;
-                });
+                }
+                );
         }
 })
