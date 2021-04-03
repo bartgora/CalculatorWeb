25d9507c41a0b9427d9aa7dd33c18fcf7cd91f47
bartlomiej-gora <bartlomiej.gora@gmail.com>
2021-04-03 15:25:05 +0200

    Open API spec added

--- src/main/java/pl/bgora/calculator/web/rest/RestCalculatorController.java
+++ src/main/java/pl/bgora/calculator/web/rest/RestCalculatorController.java
@@ -1,11 +1,14 @@
 package pl.bgora.calculator.web.rest;
 
 import com.github.bgora.rpnlibrary.exceptions.RPNException;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import pl.bgora.calculator.web.model.CalculationResponse;
@@ -25,11 +28,12 @@ public class RestCalculatorController {
         this.webCalculatorDecorator = webCalculatorDecorator;
     }
 
-    @RequestMapping("/calculate/{input}")
+    @GetMapping("/calculate/{input}")
+    @ApiResponse(content = @Content(schema = @Schema(implementation = CalculationResponse.class)))
     public Mono<CalculationResponse> calculate(@PathVariable String input) throws RPNException {
         return Mono.just(webCalculatorDecorator.calculate(input))
                 .map(BigDecimal::toString)
-                .map(result-> new CalculationResponse(input, result));
+                .map(result -> new CalculationResponse(input, result));
     }
 
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
